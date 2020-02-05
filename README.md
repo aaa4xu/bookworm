40f57350fc9d8a54a16a0856c3927d771759b901
aaa4xu <aaa4xu@gmail.com>
2020-02-05 12:23:36 +0300

    ✨ User-friendly npm start command

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Bookwalker client for Node.js
 
-## Progress
-* [x] Config decoding
-* [x] Image decoding
-* [x] HTTP client
+## Example
+1. My Library > Read this book
+2. Get content id from url
+    - member.bookwalker.jp/app/03/webstore/cooperation?r=BROWSER_VIEWER/**xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx**/https%3A%2F%2Fglobal.bookwalker.jp%2FholdBooks%2F
+    - viewer.bookwalker.jp/03/10/viewer.html?cid=**xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx**&cty=1
+3. `npm start`
+
+## Env variables
+- `BW_THROTTLE` `default: 12` - Throttle http requests per minute to bookwalker
+- `BW_DONT_SAVE_AUTH` `default: 0` - Disable saving username/password to `auth.json`
+- `BW_CONTENT_ID` - Non-interactive way to set contentId
+- `BW_USERNAME` - Non-interactive way to set username
+- `BW_PASSWORD` - Non-interactive way to set password
+- `BW_BROWSER_ID` - Non-interactive way to set browser id
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,74 @@
 import { promises as fs } from 'fs';
+import path from 'path';
+import readlineSync from 'readline-sync';
 import Book from './Book';
 import HttpClient from './Http/Client';
 import Login from './Http/Requests/Login';
 import StartSession from './Http/Requests/StartSession';
 
+const readJson = <T>(filename: string): Promise<T> => fs.readFile(filename, 'utf8').then(json => JSON.parse(json));
+const writeJson = <T>(filename: string, content: T) => fs.writeFile(filename, JSON.stringify(content, null, 2));
+
+const AUTH_DATA_PATH = './auth.json';
+const BROWSER_ID_PATH = './browser-id.txt';
+const STORAGE_PATH = './storage';
+
 async function main() {
-    if (!process.env.BW_USERNAME || !process.env.BW_PASSWORD) {
-        throw new Error('BW_USERNAME or BW_PASSWORD is empty');
+    const httpClient = new HttpClient();
+    const browserId = process.env.BW_BROWSER_ID || (await fs.readFile(BROWSER_ID_PATH, 'utf8').catch(() => null));
+    if (browserId) {
+        console.log(`Browser ID is detected: ${browserId}`);
+        httpClient.browserId = browserId;
     }
 
-    // My Library > Read this book
-    // https://member.bookwalker.jp/app/03/webstore/cooperation?r=BROWSER_VIEWER/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/https%3A%2F%2Fglobal.bookwalker.jp%2FholdBooks%2F
-    const contentId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+    let username, password;
+    if (process.env.BW_USERNAME && process.env.BW_PASSWORD) {
+        username = process.env.BW_USERNAME;
+        password = process.env.BW_PASSWORD;
+    } else {
+        const auth = await readJson<IAuthData>(AUTH_DATA_PATH).catch(() => null);
 
-    const httpClient = new HttpClient();
+        if (auth && auth.username && auth.password) {
+            username = auth.username;
+            password = auth.password;
+        } else {
+            username = readlineSync.question('Username: ');
+            password = readlineSync.question('Password: ', { hideEchoBack: true });
+        }
+    }
 
+    console.log(`Auth is detected: ${username}`);
     await new StartSession().execute(httpClient);
-    if (!(await new Login(process.env.BW_USERNAME, process.env.BW_PASSWORD).execute(httpClient))) {
+    if (!(await new Login(username, password).execute(httpClient))) {
         throw new Error('Incorrect username/password');
     }
     console.log('Session started');
 
+    if (process.env.BW_DONT_SAVE_AUTH !== '1') {
+        writeJson<IAuthData>(AUTH_DATA_PATH, { username, password })
+            .then(() => {
+                console.log(`Auth data saved to ${AUTH_DATA_PATH}`);
+            })
+            .catch(err => {
+                console.warn(`Failed to save auth data to ${AUTH_DATA_PATH}:`, err.stack);
+            });
+    }
+
+    const contentId = process.env.BW_CONTENT_ID || readlineSync.question('Content ID: ');
     const book = await Book.load(contentId, httpClient);
     console.log('Book loaded');
 
-    await book.download('./storage/dl');
+    const target = path.join(STORAGE_PATH, contentId);
+    await fs.mkdir(target);
+    await book.download(target);
     console.log('Done!');
 
-    await fs.writeFile('./browser-id.txt', httpClient.browserId);
+    await fs.writeFile(BROWSER_ID_PATH, httpClient.browserId);
 }
 
 main();
+
+interface IAuthData {
+    username: string;
+    password: string;
+}
