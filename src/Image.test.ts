f3e1d81610c8f3efbf53b1a4796a47b0c5491a54
aaa4xu <aaa4xu@gmail.com>
2020-02-21 02:59:37 +0300

    ✅ Test case for page with black strip from right side

--- src/Image.test.ts
+++ src/Image.test.ts
@@ -2,6 +2,20 @@ import { expect } from 'chai';
 import { promises as fs } from 'fs';
 import path from 'path';
 import Image from './Image';
+import sfs from 'fs';
+import { KeyObject } from 'crypto';
+import { loadImage } from 'canvas';
+
+const ifFixtureExists = (path: string) => {
+    try {
+        sfs.statSync(path);
+        return { it };
+    } catch(err) {
+        return { it: it.skip };
+    }
+}
+
+const NOT_EVEN_IMAGE = path.join(__dirname, '__fixtures__', 'non-even-image.json');
 
 describe('Image', () => {
     it(`should correct encode image`, async () => {
@@ -32,5 +46,14 @@ describe('Image', () => {
         expect(await image.decode()).be.deep.equal(
             await fs.readFile(path.join(__dirname, '__fixtures__', 'image-001-decoded.png')),
         );
+    });
+
+    ifFixtureExists(NOT_EVEN_IMAGE).it('should crop not even image', async () => {
+        const imageConfig = JSON.parse(await fs.readFile(NOT_EVEN_IMAGE, 'utf8'));
+        const imageBuffer = Buffer.from(imageConfig.sourceImage);
+        const image = new Image(imageBuffer, imageConfig.page);
+        const decodedImage = await image.decode();
+        const imageInfo = await loadImage(decodedImage);
+        expect(imageInfo.width).be.equal(1801);
     });
 });
